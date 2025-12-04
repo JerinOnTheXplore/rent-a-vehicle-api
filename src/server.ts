009b1bc2faff862ce09676abd420043aac5e8d67
JerinXplore <jerinjerin101325@gmail.com>
2025-12-05 01:35:48 +0600

    Used postgreSQL as db

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,24 @@
-import express, { Request, Response } from "express"
-const app = express()
-const port = 5000
+import express, { Request, Response } from "express";
+import {Pool} from "pg";
+const app = express();
+const port = 5000;
+
+
+
+app.use(express.json());
 
 app.get('/', (req:Request, res:Response) => {
   res.send('Welcome to Assignment 2!! - Vehicle Rental System API is running..')
-})
+});
+
+app.post("/",(req:Request, res:Response)=>{
+    console.log(req.body);
+
+    res.status(201).json({
+        success:true,
+        message: "API is working",
+    });
+});
 
 app.listen(port, () => {
   console.log(`Vehicle Rental System Server listening on port ${port}`)
